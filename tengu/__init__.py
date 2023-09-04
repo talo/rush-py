4a4d683d2509275491fe96c1741989c562e064fd
Ryan Swart <ryan@talo.systems>
2023-09-04 16:01:09 +0800

    fix: remove code broken in python3.11

--- tengu/__init__.py
+++ tengu/__init__.py
@@ -1,6 +1,5 @@
 """Main entrypoint into package."""
-from . import data
 from .api import Provider, Arg
 from .local import LocalProvider
 
-__all__ = ["Arg", "LocalProvider", "Provider", "data"]
+__all__ = ["Arg", "LocalProvider", "Provider"]
