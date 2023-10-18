f42c143704ecea11a528ba19a7f9b5d19966d53e
Sean L <sean@talolabs.com>
2023-10-18 18:33:33 +0800

    chore: Clean up flake and remove license (for now)

--- flake.nix
+++ flake.nix
@@ -4,26 +4,26 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     talo-flake-parts.url = "github:talo/talo-flake-parts";
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = inputs@{ flake-parts, talo-flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ talo-flake-parts.flakeModule ];
-      systems = [ "x86_64-linux" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        poetry2nix = inputs.poetry2nix.legacyPackages.${system};
-        poetryProjects.default = {
-          projectDir = ./.;
-          overrides = inputs.talo-flake-parts.lib.withPoetryOverrides
-            (self: super: { pip = pkgs.python3Packages.pip; });
-          extraPackages = [
-            pkgs.quarto
-          ];
-        };
+  outputs = inputs@{ flake-parts, talo-flake-parts, ... }: flake-parts.lib.mkFlake {
+    inherit inputs;
+  } {
+    imports = [ talo-flake-parts.flakeModule ];
+    systems = [ "x86_64-linux" "aarch64-darwin" ];
+    perSystem = { config, self', inputs', pkgs, system, ... }: {
+      poetryProjects.default = {
+        projectDir = ./.;
+        flake8Check = { enable = false; };
+        pyrightCheck = { enable = false; };
+        pytestCheck = { enable = false; };
+        overrides = inputs.talo-flake-parts.lib.withPoetryOverrides(self: super: {
+          pip = pkgs.python3Packages.pip;
+        });
+        extraPackages = [
+          pkgs.quarto
+        ];
       };
     };
+  };
 }
