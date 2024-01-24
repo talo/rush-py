7cbb3abdcba11f1377edbe87a6854417e4f66da3
Ryan Swart <ryan@talo.systems>
2024-01-24 10:56:52 +0800

    fix: re-run notebooks after rename

--- rush/doc.py
+++ rush/doc.py
@@ -5,8 +5,8 @@ from . import provider
 
 _x = provider.Provider()
 fns = asyncio.run(_x.get_module_functions())
-TenguProvider = type(
-    "TenguProvider",
+RushProvider = type(
+    "RushProvider",
     (provider.Provider,),
     fns | {n: getattr(_x, n) for n in dir(_x)},
 )
