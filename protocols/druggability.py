89856e5766b560d529c0c9000971186b4e0f8de0
Sean L <sean.laguna@qdx.co>
2024-05-15 18:58:47 +0800

    feat: add PDB crossrefs to druggability protocol
      - whole pipeline done by target gene now, not Uniprot ID
      - fetch UniProt data based on target gene
      - fetch PDBs for cross-referenced structures
      - run prepare_protein and p2rank on each of those PDBs
      - download everything

--- protocols/druggability.py
+++ protocols/druggability.py
@@ -1,10 +1,31 @@
+import gzip
 import json
 from dataclasses import dataclass
 from pathlib import Path
 
 import datargs
+import httpx
 
 import rush
+
+
+@dataclass
+class CrossRef:
+    database: str
+    id: str
+    method: str
+    resolution: str
+    chains: str
+
+
+@dataclass
+class UniprotData:
+    entry_type: str
+    primary_accession: str
+    sequence: str
+    molecular_weight: int
+    cross_refs: list[CrossRef]
+
 
 # Parameters
 
@@ -30,7 +51,53 @@ COLABFOLD_FOLD_RUN_CONFIG = {
     "target": "GADI",
 }
 
+PREPARE_PROTEIN_RUN_CONFIG = {}
+
 P2RANK_RUN_CONFIG = {}
+
+
+def get_uniprot(target: str):
+    params = {
+        "query": f"organism_name:Human AND reviewed:true AND gene:{target}",
+        "fields": "xref_pdb,sequence",
+        "format": "json",
+    }
+    response = httpx.get("https://rest.uniprot.org/uniprotkb/stream", params=params)
+    response_json = response.json()
+    uniprot_data = []
+    for result in response_json["results"]:
+        cross_refs = []
+        for cross_ref_data in result["uniProtKBCrossReferences"]:
+            method = ""
+            resolution = ""
+            chains = ""
+            for property in cross_ref_data["properties"]:
+                if property["key"] == "Method":
+                    method += property["value"]
+                if property["key"] == "Resolution":
+                    resolution += property["value"]
+                if property["key"] == "Chains":
+                    chains += property["value"]
+            cross_refs.append(
+                CrossRef(cross_ref_data["database"], cross_ref_data["id"], method, resolution, chains)
+            )
+        uniprot_data.append(
+            UniprotData(
+                result["entryType"],
+                result["primaryAccession"],
+                result["sequence"]["value"],
+                result["sequence"]["molWeight"],
+                cross_refs,
+            )
+        )
+    assert len(uniprot_data) == 1
+    return uniprot_data[0]
+
+
+def get_pdb(id: str):
+    print(f"https://files.rcsb.org/download/{id}.pdb.gz")
+    response = httpx.get(f"https://files.rcsb.org/download/{id}.pdb.gz", timeout=30.0)
+    return str(gzip.decompress(response.content), "utf-8")
 
 
 def read_fasta(filepath: Path) -> dict[str, str]:
@@ -70,28 +137,27 @@ def read_fasta(filepath: Path) -> dict[str, str]:
 
 @dataclass
 class Args:
-    input_sequence: Path
+    target: str
 
 
 def main():
     args = datargs.parse(Args)
 
     # 1.0: Obtain input (FASTA)
-    fasta_name, fasta_sequence = next(iter(read_fasta(args.input_sequence).items()))
-    print(f"{fasta_name=}")
-    print(f"{fasta_sequence=}")
-    uniprot_id = fasta_name.split("|")[1]
+    uniprot_data = get_uniprot(args.target)
+    uniprot_id = uniprot_data.primary_accession
     print(f"{uniprot_id=}")
+    print(f"{uniprot_data.sequence=}")
 
     client = rush.build_blocking_provider_with_functions(
-        batch_tags=["paratus", "protocol", "druggability", uniprot_id],
+        batch_tags=["paratus", "protocol", "druggability", uniprot_data.primary_accession],
         workspace=WORK_DIR,
         restore_by_default=True,
     )
 
     # 1.1: MSA
     (msa_handle,) = client.mmseqs2(
-        {"fasta": [fasta_sequence]},
+        {"fasta": [uniprot_data.sequence]},
         **COLABFOLD_SEARCH_RUN_CONFIG,
     )
 
@@ -100,9 +166,9 @@ def main():
         msa_handle,
         **COLABFOLD_FOLD_RUN_CONFIG,
     )
+    (folded_conformer_handle,) = client.pick_conformer(folded_conformers_handle, 0)
 
     # 2.0: Evaluate pockets
-    (folded_conformer_handle,) = client.pick_conformer(folded_conformers_handle, 0)
     (p2rank_prediction_handle, pymol_viz_handle) = client.p2rank(
         folded_conformer_handle,
         True,
@@ -124,6 +190,44 @@ def main():
         filename=f"{uniprot_id}_2.0_pocket_viz.tar.gz",
         overwrite=True,
     )
+
+    for cross_ref in uniprot_data.cross_refs:
+        # 1.2: Get 3D structure from cross-referenced data
+        pdb_path = WORK_DIR / "objects" / f"{uniprot_id}-{cross_ref.id}_1.2_structure.pdb"
+        if not pdb_path.is_file():
+            pdb_path.write_text(get_pdb(cross_ref.id))
+
+        # 1.3: Clean up structure
+        (prepared_structures_handle, _) = client.prepare_protein(
+            pdb_path,
+            None,
+            None,
+            tags=[cross_ref.id],
+            **PREPARE_PROTEIN_RUN_CONFIG,
+        )
+        (prepared_structure_handle,) = client.pick_conformer(
+            prepared_structures_handle, 0, tags=[cross_ref.id]
+        )
+
+        # 2.0: Evaluate pockets
+        (p2rank_prediction_handle, pymol_viz_handle) = client.p2rank(
+            prepared_structure_handle,
+            True,
+            tags=[cross_ref.id],
+            **P2RANK_RUN_CONFIG,
+        )
+
+        # 2.1: Download everything & show evaluation
+        prepared_structure_handle.download(
+            filename=f"{uniprot_id}-{cross_ref.id}_1.3_prepared.json",
+            overwrite=True,
+        )
+        with open(WORK_DIR / "objects" / f"{uniprot_id}-{cross_ref.id}_2.0_pocket_data.json", "w") as f:
+            json.dump(p2rank_prediction_handle.get(), f, indent=2)
+        pymol_viz_handle.download(
+            filename=f"{uniprot_id}-{cross_ref.id}_2.0_pocket_viz.tar.gz",
+            overwrite=True,
+        )
 
 
 if __name__ == "__main__":
