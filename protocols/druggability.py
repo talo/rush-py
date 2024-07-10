5f1f4d301506781b884f318ce3f84b2ad6c7f9d2
Sean L <sean.laguna@qdx.co>
2024-07-10 15:09:36 -0500

    feat: score computation, etc. (from a while back)

--- protocols/druggability.py
+++ protocols/druggability.py
@@ -1,15 +1,83 @@
 #!/usr/bin/env python3
 
+import dataclasses
 import gzip
 import json
-from dataclasses import dataclass
+from dataclasses import dataclass, is_dataclass
 from pathlib import Path
 from typing import Literal
 
 import datargs
 import httpx
+import prettyprinter
 
 import rush
+
+from blosum import BLOSUM62
+from paratus import QDX_AMISS_PATHOGENIC
+# from prettyprinter import pprint
+
+prettyprinter.install_extras(exclude=["ipython", "django"])
+
+
+def aligned_index(seq_a, i_u):
+    """
+    seq_a is the aligned sequence (with dashes)
+    i_u is the index into the unaligned sequence so 113 / 129 / 157 for our cases since the index starts at 0
+    output (i_a, seq_a[i_a]) are the aligned indices and the amino acid at the index, respectively
+    """
+    seq_u = seq_a.replace("-", "")
+    walk_a = 0
+    walk_u = 0
+    while walk_u != i_u or seq_a[walk_a] != seq_u[i_u]:
+        if seq_a[walk_a] == "-":
+            walk_a += 1
+        elif seq_a[walk_a] == seq_u[walk_u]:
+            walk_a += 1
+            walk_u += 1
+    i_a = walk_a
+    assert i_u == walk_u
+    assert i_u <= i_a
+    assert seq_u[i_u] == seq_a[i_a]
+    return (i_a, seq_a[i_a])
+
+
+def unaligned_index(seq_a, i_a):
+    return len(seq_a[: i_a + 1].replace("-", ""))
+
+
+AA_1TO3 = {
+    "C": "CYS",
+    "D": "ASP",
+    "S": "SER",
+    "Q": "GLN",
+    "K": "LYS",
+    "I": "ILE",
+    "P": "PRO",
+    "T": "THR",
+    "F": "PHE",
+    "N": "ASN",
+    "G": "GLY",
+    "H": "HIS",
+    "L": "LEU",
+    "R": "ARG",
+    "W": "TRP",
+    "A": "ALA",
+    "V": "VAL",
+    "E": "GLU",
+    "Y": "TYR",
+    "M": "MET",
+}
+
+
+class EnhancedJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if is_dataclass(o):
+            return dataclasses.asdict(o)
+        elif isinstance(o, set):
+            return sorted(list(o))
+        return super().default(o)
+
 
 #### These are for UniProt information cross refs, etc. about the target
 
@@ -27,6 +95,7 @@ class UniprotData:
     entry_type: str
     primary_accession: str
     sequence: str
+    aligned_sequence: str
     molecular_weight: int
     pdb_cross_refs: list[CrossRefPDB]
 
@@ -60,7 +129,7 @@ class Interactor:
 @dataclass
 class BindingInteraction:
     interaction_id: str
-    target_id: str
+    uniprot_id: str
     target_seq_range: set[tuple[int, int] | int]
     # domain_name: str | None  # not always available
     interactors: list[Interactor]
@@ -97,35 +166,6 @@ class BindingSite:
     details: str
 
 
-# Parameters
-
-WORK_DIR = Path.cwd()
-
-COLABFOLD_SEARCH_RUN_CONFIG = {
-    "resources": {
-        "gpus": 0,
-        "cpus": 48,
-        "mem": 128 * 1024,
-        "storage_mounts": ["gdata/if89"],
-        "walltime": 360,
-    },
-    "target": "GADI",
-}
-
-COLABFOLD_FOLD_RUN_CONFIG = {
-    "resources": {
-        "gpus": 1,
-        "storage_mounts": ["gdata/if89"],
-        "walltime": 360,
-    },
-    "target": "GADI",
-}
-
-PREPARE_PROTEIN_RUN_CONFIG = {}
-
-P2RANK_RUN_CONFIG = {}
-
-
 def get_uniprot(target: str):
     params = {
         "query": f"organism_name:Human AND reviewed:true AND gene:{target}",
@@ -157,6 +197,7 @@ def get_uniprot(target: str):
                 result["entryType"],
                 result["primaryAccession"],
                 result["sequence"]["value"],
+                result["sequence"]["value"],
                 result["sequence"]["molWeight"],
                 pdb_cross_refs,
             )
@@ -186,14 +227,14 @@ def parse_range(idx_range) -> list[tuple[int, int] | int]:
     return result
 
 
-def get_binding_ixns_from_intact(target_id: str) -> list[BindingInteraction]:
+def get_binding_ixns_from_intact(uniprot_id: str) -> list[BindingInteraction]:
     params = {
         "advancedSearch": "true",
         "interactionTypesFilter": "direct interaction",
         "interactorSpeciesFilter": "Homo sapiens",
         "intraSpeciesFilter": "true",
         "negativeFilter": "POSITIVE_ONLY",
-        "query": f"id:{target_id}",
+        "query": f"id:{uniprot_id}",
     }
     response = httpx.post(
         "https://www.ebi.ac.uk/intact/ws/interaction/findInteractionWithFacet", params=params
@@ -254,11 +295,11 @@ def get_binding_ixns_from_intact(target_id: str) -> list[BindingInteraction]:
             binding_interactions.append(
                 BindingInteraction(
                     interaction_id,
-                    target_id,
+                    uniprot_id,
                     target_seq_range=set(
                         sequence_range
                         for participant in interaction_participants[k]
-                        if participant.id == f"uniprotkb_{target_id}"
+                        if participant.id == f"uniprotkb_{uniprot_id}"
                         for sequence_range in participant.sequence_ranges
                     ),
                     interactors=[
@@ -277,7 +318,29 @@ def get_binding_ixns_from_intact(target_id: str) -> list[BindingInteraction]:
     return binding_interactions
 
 
-def get_binding_ixns_from_pdbe(pdb_cross_refs: list[str]) -> list[BindingSite]:
+@dataclass
+class BindingPocket:
+    target_seq_range: set[tuple[int, int] | int]
+    center: tuple[float, float, float]
+    probability: float
+
+
+def get_binding_regions_from_p2rank(uniprot_id: str, probability_threshold: float) -> list[BindingPocket]:
+    with open(WORK_DIR / "objects" / f"{uniprot_id}_2.0_pocket_data.json") as f:
+        p2rank_data = json.load(f)
+
+    return [
+        BindingPocket(
+            {int(residue_id.split("_")[1]) for residue_id in pocket["residue_ids"]},
+            (pocket["center_x"], pocket["center_y"], pocket["center_z"]),
+            pocket["probability"],
+        )
+        for pocket in p2rank_data["pockets"]
+        if pocket["probability"] >= probability_threshold
+    ]
+
+
+def get_binding_sites_from_pdbe(pdb_cross_refs: list[str]) -> list[BindingSite]:
     binding_sites = []
     # for pdb_cross_ref_id in ["3RN2", "3RN5", "3VD8", "4O7Q", "6MB2", "7K3R"]:
     for pdb_cross_ref in pdb_cross_refs:
@@ -349,7 +412,9 @@ def compute_percent_overlapping(reference_range, query_range) -> float:
     return sum(1 if query_idx in reference_idxs else 0 for query_idx in query_idxs) / len(reference_idxs)
 
 
-def compute_scores(region_of_interest, intact_binding_ixns, pdbe_binding_sites) -> tuple[float, float, float]:
+def compute_scores(
+    region_of_interest, intact_binding_ixns, p2rank_binding_pockets, pdbe_binding_sites
+) -> tuple[float, float, float, float]:
     # ligand <-> region of interest
     lig_roi_score = 0.0
     for binding_site in pdbe_binding_sites:
@@ -369,18 +434,19 @@ def compute_scores(region_of_interest, intact_binding_ixns, pdbe_binding_sites) 
             if pct_overlap > 0.0:
                 lig_bixn_score += 1 + pct_overlap
 
-    # # ligand <-> binding interaction regions (predicted)
-    # lig_breg_score = 0.0
-    # for binding_site in pdbe_binding_sites:
-    #     for binding_region in p2rank_binding_regions:
-    #         reference_seq_range = binding_region.target_seq_range
-    #         query_seq_range = binding_site.target_seq_range
-    #         pct_overlap = compute_percent_overlapping(reference_seq_range, query_seq_range)
-    #         if pct_overlap > 0.0:
-    #             lig_breg_score += 1 + pct_overlap
+    # ligand <-> binding pockets (predicted)
+    lig_bpocket_score = 0.0
+    for binding_site in pdbe_binding_sites:
+        for binding_region in p2rank_binding_pockets:
+            reference_seq_range = binding_region.target_seq_range
+            query_seq_range = binding_site.target_seq_range
+            pct_overlap = compute_percent_overlapping(reference_seq_range, query_seq_range)
+            if pct_overlap > 0.0:
+                lig_bpocket_score += 1 + pct_overlap
 
     # ligand <-> other ligands
     lig_lig_score = 0.0
+    # used = {}
     for i in range(len(pdbe_binding_sites)):
         for j in range(i, len(pdbe_binding_sites)):
             seq_range_i = pdbe_binding_sites[i].target_seq_range
@@ -391,8 +457,20 @@ def compute_scores(region_of_interest, intact_binding_ixns, pdbe_binding_sites) 
             ) / 2
             if avg_pct_overlap > 0.0:
                 lig_lig_score += 1 + avg_pct_overlap
+                # Use each identical pair only once
+                # k = tuple((
+                #     pdbe_binding_sites[i].pdb_id,
+                #     frozenset(ligand.residue_name for ligand in pdbe_binding_sites[i].ligands),
+                #     pdbe_binding_sites[j].pdb_id,
+                #     frozenset(ligand.residue_name for ligand in pdbe_binding_sites[j].ligands),
+                # ))
+                # if k not in used:
+                #     used[k] = avg_pct_overlap
+                #     lig_lig_score += 1 + avg_pct_overlap
+                # else:
+                #     print(f"reusing {k}!")
 
-    return (lig_roi_score, lig_bixn_score, lig_lig_score)
+    return (lig_roi_score, lig_bixn_score, lig_bpocket_score, lig_lig_score)
 
 
 def read_fasta(filepath: Path) -> dict[str, str]:
@@ -430,6 +508,56 @@ def read_fasta(filepath: Path) -> dict[str, str]:
         return fasta_sequences
 
 
+def fasta_from_pdb(pdb_str):
+    from Bio import PDB
+    from Bio.Seq import Seq
+    from Bio.SeqRecord import SeqRecord
+
+    structure = pdb_str
+    models = []
+    for model in structure:
+        chains = []
+        for chain in model:
+            sequence = ""
+            for residue in chain:
+                if PDB.is_aa(residue):
+                    sequence += PDB.Polypeptide.three_to_one(residue.get_resname())
+            seq_record = SeqRecord(Seq(sequence), id=chain.id)
+            chains.append(seq_record.format("fasta"))
+        models.append(chains)
+
+    return models
+
+
+#### Parameters
+
+WORK_DIR = Path.cwd()
+
+COLABFOLD_SEARCH_RUN_CONFIG = {
+    "resources": {
+        "gpus": 0,
+        "cpus": 48,
+        "mem": 128 * 1024,
+        "storage_mounts": ["gdata/if89"],
+        "walltime": 360,
+    },
+    "target": "GADI",
+}
+
+COLABFOLD_FOLD_RUN_CONFIG = {
+    "resources": {
+        "gpus": 1,
+        "storage_mounts": ["gdata/if89"],
+        "walltime": 360,
+    },
+    "target": "GADI",
+}
+
+PREPARE_PROTEIN_RUN_CONFIG = {}
+
+P2RANK_RUN_CONFIG = {}
+
+
 @dataclass
 class Args:
     target: str
@@ -441,8 +569,48 @@ def main():
 
     #### Starting with UniProt seqs
 
-    # 1.0: Obtain input (FASTA)
-    uniprot_data = get_uniprot(args.target)
+    if args.target == "paratus_seqs":
+        with open(Path.cwd() / "paratus_data" / "paratus_seqs.json") as f:
+            paratus_seqs = json.load(f)
+        uniprot_data = {}
+        for seq in paratus_seqs["seqs"]:
+            raw_name = "".join(c if (c.isalnum() or c in ("-")) else "_" for c in seq["name"])
+            raw_seq = seq["seq"].replace("-", "")
+            # if (
+            #     raw_seq
+            #     == "MVRMVPVLLSLLLLLGPAVPQENQDGRYSLTYIYTGLSKHVEDVPAFQALGSLNDLQFFRYNSKDRKSQPMGLWRQVEGMEDWKQDSQLQKAREDIFMETLKDIVEYYNDSNGSHVLQGRFGCEIENNRSSGAFWKYYYDGKDYIEFNKEIPAWVPFDPAAQITKQKWEAEPVYVQRAKAYLEEECPATLRKYLKYSKNILDRQDPPSVVVTSHQAPGEKKKLKCLAYDFYPGKIDVHWTRAGEVQEPELRGDVLHNGNGTYQSWVVVAVPPQDTAPYSCHVQHSSLAQPLVVPWEAS"
+            # ):
+            #     print(aligned_index(seq["seq"], 157))
+            for residue_num in QDX_AMISS_PATHOGENIC.keys():
+                print(
+                    f"{raw_name},{unaligned_index(seq["seq"], residue_num - 1)},{seq["seq"][residue_num - 1]}"
+                )
+            # print(raw_name)
+            # print(raw_seq)
+            uniprot_data[raw_name] = UniprotData("", raw_name, raw_seq, seq["seq"], 0, [])
+        # uniprot_data = uniprot_data[0]
+    else:
+        # 1.0: Obtain input (FASTA)
+        uniprot_data = get_uniprot(args.target)
+
+    print(len(uniprot_data))
+    # mAetAle1_1-291
+    # mCynHor1_1-291
+    # mArtInt1_1-290
+    # mArtLit_1-290
+    # mCenSen1_1-293
+    # mPlaHel1_1-293
+    # mStuPar1_1-292
+    # mUroCon1_1-293
+    # mLopEvo1_1-293
+    uniprot_data = uniprot_data["pub_GRCh38_1-298"]
+
+    for residue_number, mutations in QDX_AMISS_PATHOGENIC.items():
+        for m in mutations:
+            w = uniprot_data.aligned_sequence[residue_number - 1]
+            if m != w:
+                print(f"{residue_number},{w},{m},{BLOSUM62[w][m]}")
+
     uniprot_id = uniprot_data.primary_accession
     print(f"{uniprot_id=}")
     print(f"{uniprot_data.sequence=}")
@@ -460,7 +628,7 @@ def main():
     )
 
     # 1.2: Produce 3D structure
-    (folded_conformers_handle,) = client.colabfold_fold(
+    (folded_conformers_handle, folded_pdbs_handle) = client.colabfold_fold(
         msa_handle,
         **COLABFOLD_FOLD_RUN_CONFIG,
     )
@@ -482,6 +650,10 @@ def main():
         filename=f"{uniprot_id}_1.2_conformer.json",
         overwrite=True,
     )
+    folded_pdbs_handle.download(
+        filename=f"{uniprot_id}_alphafold.tar.gz",
+        overwrite=True,
+    )
     with open(WORK_DIR / "objects" / f"{uniprot_id}_2.0_pocket_data.json", "w") as f:
         json.dump(p2rank_prediction_handle.get(), f, indent=2)
     pymol_viz_handle.download(
@@ -489,9 +661,12 @@ def main():
         overwrite=True,
     )
 
+    """
     #### Starting with cross-refs (i.e. PDB files)
 
     for pdb_cross_ref in uniprot_data.pdb_cross_refs:
+        if pdb_cross_ref.id == "3DLS":
+            continue
         # 1.2: Get 3D structure from cross-referenced data
         pdb_path = WORK_DIR / "objects" / f"{uniprot_id}-{pdb_cross_ref.id}_1.2_structure.pdb"
         if not pdb_path.is_file():
@@ -509,7 +684,7 @@ def main():
             prepared_structures_handle, 0, tags=[pdb_cross_ref.id]
         )
 
-        # 2.0: Evaluate pockets
+        # # 2.0: Evaluate pockets
         (p2rank_prediction_handle, pymol_viz_handle) = client.p2rank(
             prepared_structure_handle,
             True,
@@ -529,23 +704,52 @@ def main():
             overwrite=True,
         )
 
+    """
+
     #### Binding site analysis
 
+    """
     region_of_interest = set((0, len(uniprot_data.sequence)))
     if args.region_of_interest:
         region_of_interest = parse_range(args.region_of_interest)
 
-    # 3.1: Get IntAct data (mostly Protein - Protein, Protein - DNA/RNA, etc.)
+    # 3.0a: Get IntAct data (mostly Protein - Protein, Protein - DNA/RNA, etc.)
     intact_binding_ixns = get_binding_ixns_from_intact(uniprot_id)
-    print(intact_binding_ixns)
+    with open(WORK_DIR / "objects" / f"{uniprot_id}_3.0a_intact_binding_ixns.json", "w") as f:
+        json.dump(intact_binding_ixns, f, indent=2, cls=EnhancedJSONEncoder)
+    """
+    # 3.0b: Parse P2Rank data (predicted potential binding pockets)
+    p2rank_pockets = get_binding_regions_from_p2rank(uniprot_id, probability_threshold=0.25)
+    with open(WORK_DIR / "objects" / f"{uniprot_id}_3.0b_p2rank_pockets.json", "w") as f:
+        json.dump(p2rank_pockets, f, indent=2, cls=EnhancedJSONEncoder)
+    """
+    # 3.0c: Get PDB binding site data (mostly Protein - Ligand/SMOL)
+    pdbe_binding_sites = get_binding_sites_from_pdbe(uniprot_data.pdb_cross_refs)
+    with open(WORK_DIR / "objects" / f"{uniprot_id}_3.0c_pdbe_binding_sites.json", "w") as f:
+        json.dump(pdbe_binding_sites, f, indent=2, cls=EnhancedJSONEncoder)
 
-    # 3.2: Get PDB binding site data (mostly Protein - Ligand/SMOL)
-    pdbe_binding_sites = get_binding_ixns_from_pdbe(uniprot_data.pdb_cross_refs)
-    print(pdbe_binding_sites)
-
-    # # 3.3: Compute scores
-    scores = compute_scores(region_of_interest, intact_binding_ixns, pdbe_binding_sites)
-    print(scores)
+    # 3.1: Compute scores
+    roi_score, bixn_score, bpocket_score, bsite_score = compute_scores(
+        region_of_interest,
+        intact_binding_ixns,
+        p2rank_pockets,
+        pdbe_binding_sites,
+    )
+    score_dict = {
+        "region_of_interest_score": round(roi_score, 2),
+        "functional_domain_score": round(bixn_score, 2),
+        "binding_pocket_score": round(bpocket_score, 2),
+        "ligandability_score": round(bsite_score, 2),
+    }
+    print(json.dumps(score_dict, indent=2))
+    with open(WORK_DIR / "objects" / f"{uniprot_id}_3.1_druggability_score.json", "w") as f:
+        json.dump(
+            score_dict,
+            f,
+            indent=2,
+            cls=EnhancedJSONEncoder,
+        )
+    """
 
 
 if __name__ == "__main__":
