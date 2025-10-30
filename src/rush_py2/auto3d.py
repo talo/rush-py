4301c632db4c2b4bf1ba230036fc20c1b5083e71
loong <bzlw@pm.me>
2025-10-30 11:50:39 +0800

    feat: support auto3d options

--- src/rush_py2/auto3d.py
+++ src/rush_py2/auto3d.py
@@ -1,36 +1,73 @@
+from dataclasses import dataclass
 from string import Template
 
 from gql.transport.exceptions import TransportQueryError
 
 from rush_py2.client import PROJECT_ID, RunSpec, print_run_trace, submit_rex
+from rush_py2.utils import bool_to_str
 
+@dataclass
+class Auto3DOptions:
+    k = 1
+    batchsize_atoms = 1024
+    capacity = 40
+    convergence_threshold = 0.003
+    enumerate_isomer = True
+    enumerate_tautomer = False
+    job_name = ""
+    max_confs = None
+    memory = None
+    mpi_np = 4
+    opt_steps = 5000
+    patience = 1000
+    threshold = 0.3
+
+    def to_rex(self, reference_fragment: int | None = None):
+        return Template(
+            """auto3d_rex::Auto3dOptions {
+  k = Some (int $k),
+  batchsize_atoms = Some $batchsize_atoms,
+  capacity = Some $capacity,
+  convergence_threshold = Some $convergence_threshold,
+  enumerate_isomer = Some $enumerate_isomer,
+  enumerate_tautomer = Some $enumerate_tautomer,
+  job_name = Some "$job_name",
+  max_confs = $max_confs,
+  memory = $memory,
+  mpi_np = Some $mpi_np,
+  opt_steps = Some $opt_steps,
+  optimizing_engine = Some auto3d_rex::Auto3dOptimizingEngines::AIMNET,
+  patience = Some $patience,
+  threshold = Some $threshold,
+  verbose = Some false,
+  window = None,
+}"""
+        ).substitute(
+            k=self.k,
+            batchsize_atoms=self.batchsize_atoms,
+            capacity=self.capacity,
+            convergence_threshold=self.convergence_threshold,
+            enumerate_isomer=bool_to_str(self.enumerate_isomer),
+            enumerate_tautomer=bool_to_str(self.enumerate_tautomer),
+            job_name=self.job_name,
+            max_confs=self.max_confs,
+            memory=self.memory,
+            mpi_np=self.mpi_np,
+            opt_steps=self.opt_steps,
+            patience=self.patience,
+            threshold=self.threshold,
+        )
 
 def auto3d(
     smis: list[str],
+    opts: Auto3DOptions = Auto3DOptions(),
     run_spec: RunSpec = RunSpec(),
 ):
     rex = Template("""let
   results = 
     try_auto3d_rex
       default_runspec_gpu
-      (auto3d_rex::Auto3dOptions {
-        k = Some (int 1),
-        batchsize_atoms = Some 1024,
-        capacity = Some 40,
-        convergence_threshold = Some 0.003,
-        enumerate_isomer = Some true,
-        enumerate_tautomer = Some false,
-        job_name = Some "",
-        max_confs = None,
-        memory = None,
-        mpi_np = Some 4,
-        opt_steps = Some 5000,
-        optimizing_engine = Some auto3d_rex::Auto3dOptimizingEngines::AIMNET,
-        patience = Some 1000,
-        threshold = Some 0.3,
-        verbose = Some false,
-        window = None,
-      })
+      ($opts)
       $smis
 in
   map 
@@ -43,10 +80,18 @@ in
     )
     (zip (map unwrap (unwrap (unwrap results))) $smis)""").substitute(
         smis= f"[{', '.join([f'\"{smi}\"' for smi in smis])}]",
+        opts=opts.to_rex(),
         run_spec=run_spec.to_rex(),
     )
     try:
-        return submit_rex(PROJECT_ID, rex)
+        run = submit_rex(PROJECT_ID, rex)
+        if run is not None:
+            if run["status"] == "error":
+                print_run_trace(run)
+            return run
+        else:
+            print("No run available")
+            return None
 
     except TransportQueryError as e:
         if e.errors:
--- tests/test_auto3d.py
+++ tests/test_auto3d.py
@@ -1,5 +1,6 @@
 
 # Run test
+import json
 import sys
 
 from rush_py2.auto3d import auto3d
@@ -14,5 +15,7 @@ if __name__ == "__main__":
             print(object)
             topology_data_as_json = download_object(topology_data_as_vobj["path"])
             print(topology_data_as_json)
+            topology_data = json.loads(topology_data_as_json)
+            print(topology_data)
     else:
         print("No run available")
