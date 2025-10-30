3cb1cd251b5b1fd18ce6dcf8ac361adbaac840ad
loong <bzlw@pm.me>
2025-10-30 11:34:39 +0800

    chore: fix nomeclature

--- tests/test_auto3d.py
+++ tests/test_auto3d.py
@@ -7,12 +7,12 @@ from rush_py2.client import download_object
 
 
 if __name__ == "__main__":
-    result = auto3d(["C1=CC=CC=C1"])
-    if result is not None:
-        for [smi, topology_data_as_vobj] in result["result"]:
+    run = auto3d(["C1=CC=CC=C1"])
+    if run is not None:
+        for [smi, topology_data_as_vobj] in run["result"]:
             print(smi)
             print(object)
             topology_data_as_json = download_object(topology_data_as_vobj["path"])
             print(topology_data_as_json)
     else:
-        print("No result")
+        print("No run available")
