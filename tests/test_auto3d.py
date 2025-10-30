82dc1db36aaaf7a368f4c5809ebec65bf1f0264e
loong <bzlw@pm.me>
2025-10-30 11:33:19 +0800

    chore: better example

--- tests/test_auto3d.py
+++ tests/test_auto3d.py
@@ -3,8 +3,16 @@
 import sys
 
 from rush_py2.auto3d import auto3d
+from rush_py2.client import download_object
 
 
 if __name__ == "__main__":
     result = auto3d(["C1=CC=CC=C1"])
-    print(result)
+    if result is not None:
+        for [smi, topology_data_as_vobj] in result["result"]:
+            print(smi)
+            print(object)
+            topology_data_as_json = download_object(topology_data_as_vobj["path"])
+            print(topology_data_as_json)
+    else:
+        print("No result")
