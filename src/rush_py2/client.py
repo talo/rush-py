714d4c20479f059296ae53789cf50d53df505e60
Sean L <sean.laguna@qdx.co>
2025-12-05 00:29:08 -0600

    feat: param to save & extract bins in save_object

--- src/rush_py2/client.py
+++ src/rush_py2/client.py
@@ -1,14 +1,17 @@
 import json
 import re
 import sys
+import tarfile
 import time
 from dataclasses import asdict, dataclass
+from io import BytesIO
 from os import getenv
 from pathlib import Path
 from string import Template
 from typing import Literal
 
 import requests
+import zstandard as zstd
 from gql import Client, FileVar, gql
 from gql.transport.requests import RequestsHTTPTransport
 
@@ -240,20 +243,50 @@ def save_json(d: dict, filepath: Path | str | None = None, name: str | None = No
     return filepath
 
 
-def save_object(path: str, filepath: Path | str | None = None, name: str | None = None):
-    d = json.loads(download_object(path).decode())
+def save_object(
+    path: str,
+    filepath: Path | str | None = None,
+    name: str | None = None,
+    type: Literal["json", "bin"] | None = None,
+    ext: str | None = None,
+    extract: bool = False,
+):
+    if type is None and (ext is None or ext == "json"):
+        type = "json"
+    else:
+        type = "bin"
+    ext = type if ext is None else ext
+
     if filepath is not None and name is None:
         if isinstance(filepath, str):
             filepath = Path(filepath)
     elif filepath is None and name is not None:
-        filepath = _get_opts().workspace_dir / PROJECT_ID / f"{name}.json"
+        filepath = _get_opts().workspace_dir / PROJECT_ID / (f"{name}." + ext)
     elif filepath is None and name is None:
-        filepath = _get_opts().workspace_dir / PROJECT_ID / f"{path}.json"
+        filepath = _get_opts().workspace_dir / PROJECT_ID / (f"{path}." + ext)
     else:
         raise Exception("Cannot specify both filepath or name")
     filepath.parent.mkdir(parents=True, exist_ok=True)
-    with open(filepath, "w") as f:
-        json.dump(clean_dict(d), f, indent=2)
+    if type == "json":
+        d = json.loads(download_object(path).decode())
+        with open(filepath, "w") as f:
+            json.dump(clean_dict(d), f, indent=2)
+    else:
+        data = download_object(path)
+        if extract:
+            decompressed = zstd.ZstdDecompressor().decompress(
+                data, max_output_size=int(1e9)
+            )
+            with tarfile.open(fileobj=BytesIO(decompressed)) as tar:
+                tar_filenames = tar.getnames()
+                if len(tar_filenames) >= 2:
+                    data = tar.extractfile(tar_filenames[1]).read()
+        if len(tar_filenames) >= 2:
+            with open(filepath, "wb") as f:
+                f.write(data)
+        else:
+            filepath.touch()
+
     return filepath
 
 
