df287378f25e8b346eb7450e236f235a91e4c233
Sean L <sean.laguna@qdx.co>
2025-12-30 18:52:23 -0800

    chore: clean up fetch_runs test

--- tests/test_fetch_runs.py
+++ tests/test_fetch_runs.py
@@ -1,4 +1,6 @@
+import sys
+
 from rush_py2.client import fetch_runs
 
 if __name__ == "__main__":
-    print(fetch_runs(name_contains="J&J"), file=sys.stderr)
+    print(fetch_runs(name_contains="Rush-Py Test"), file=sys.stderr)
