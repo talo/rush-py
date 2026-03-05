bd09f3911dd81f7d09b438443174a4b5d31bae43
Sean L <sean.laguna@qdx.co>
2026-03-05 15:36:08 -0600

    fix: increase too-low CI & test_fried_3fln timeouts

--- tests/test_fried.py
+++ tests/test_fried.py
@@ -1,12 +1,15 @@
 import json
 from pathlib import Path
 
+import pytest
+
 from rush.client import set_opts
 from rush.convert import from_pdb, from_sdf, to_json
 from rush.fried import fragment_ligand, fragmented_exess, plot_fried_stacked
 from rush import merge_trcs
 
 
+@pytest.mark.timeout(1800)
 def test_fried_3fln(tmp_path: Path):
     set_opts(workspace_dir=tmp_path / ".test-workspace")
     data_dir = Path(__file__).parent / "data"
